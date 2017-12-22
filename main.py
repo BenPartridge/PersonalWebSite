23861b60f2a434af9ee301c6e654a385ba801093
Ben Partridge <bep9614@gmail.com>
2017-12-22 01:09:56 -0600

    Final stages of content. Not happy with routing of pages, other backend things

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/profile/<name>')
-def profile(name):
-    return render_template("profile.html", name=name)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
+@app.route('/profile/')
+def profile():
+    return render_template("profile.html")
 
+
+@app.route('/profile/ben')
+def ben():
+    return render_template("ben.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
